832adf8bbaacdbcb7667372afe584bfba620cb2a
AnishOli <anisholi751106@gmail.com>
2025-04-07 18:41:16 +0800

    commit

--- Introduction to Java/Java Program/firstProgram/src/com/Anish/Inputs.java
+++ Introduction to Java/Java Program/firstProgram/src/com/Anish/Inputs.java
@@ -5,8 +5,14 @@ import java.util.Scanner;
 public class Inputs {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Please enter your RollNumber:");
-        int rollno = sc.nextInt();
-        System.out.println("Your Roll number: " + rollno);
+//        System.out.print("Please enter your RollNumber:");
+//        int rollno = sc.nextInt();
+//        System.out.println("Your Roll number: " + rollno);
+//
+//       double marks  = sc.nextDouble();
+//        System.out.println(marks);
+
+       byte a= sc.nextByte();
+        System.out.println(a);
     }
 }
